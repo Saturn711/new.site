c051ad9665f29aaa7f4c3d172fa435582f7f89a3
aleksandr <turchenyuck@mail.ru>
2015-11-16 23:24:31 +0200

    init

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<div>
+		<p>Привер, мир!</p>
+	</div>
+	<div>
+		<p>Всем привет!</p>
+	</div>
+</body>
+</html>
